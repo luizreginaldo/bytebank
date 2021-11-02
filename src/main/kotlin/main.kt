3e7eeeac3c5b3db445c54e5187082d0315ded17e
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2021-11-02 18:49:04 -0300

    for loop

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,40 @@
 fun main() {
     println("Bem vindo ao Bytebank")
+}
 
-    val holder: String = "Luiz Reginaldo"
-    val accountNumber: Int = 1000
-    var balance = 0.0
-    balance = 100 + 2.0
+fun forLoopTest() {
+    for (i in 1..3) {
+
+        if (i == 2) {
+            continue
+        }
+
+        printAccountData(i);
+    }
+
+    for (i in 3 downTo 1) {
+        printAccountData(i)
+    }
+
+    for (i in 3 until 1) {
+        printAccountData(i)
+    }
+
+    for (i in 5 downTo 1 step 2) {
+
+        if (i == 1) {
+            break
+        }
+
+        printAccountData(i)
+    }
+}
+
+fun printAccountData(value: Int) {
+    val holder: String = "Customer $value"
+    val accountNumber: Int = 1000 + value
+    var balance = value + 10.0
+    balance += 100 + 2.0
     balance += 200
 
     println("Titular: $holder")
@@ -12,16 +42,18 @@ fun main() {
     println("Saldo: $balance")
 
     conditionsTest(balance)
+
+    println("----------------------------")
 }
 
 fun conditionsTest(balance: Double) {
-    //    if (balance > 0.0) {
-    //        println("Conta é positiva")
-    //    } else if (balance == 0.0) {
-    //        println("Conta é neutra")
-    //    } else {
-    //        println("Conta é negativa")
-    //    }
+    if (balance > 0.0) {
+        println("Conta é positiva")
+    } else if (balance == 0.0) {
+        println("Conta é neutra")
+    } else {
+        println("Conta é negativa")
+    }
 
     when {
         balance > 0.0 -> println("Conta é positiva")
