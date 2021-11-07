4f4a0456610b0eee1ef693d6fbb18e8f5e2d1351
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2021-11-07 19:51:32 -0300

    override equals() and hasCode() methods

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -3,8 +3,12 @@ package br.com.alura.bytebank
 import br.com.alura.bytebank.models.Address
 
 fun main() {
-    val address = Address(address = "Rua Cinco")
+    val address = Address(address = "Rua Cinco", zipCode = "123456")
+    val newAddress = Address(address = "Rua Cinco", zipCode = "123456")
 
-    println(address)
+    println(address.hashCode())
+    println(newAddress.hashCode())
+
+//    println(address)
 }
 
