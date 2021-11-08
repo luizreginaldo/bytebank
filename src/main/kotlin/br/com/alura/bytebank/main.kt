a50bbfb77579ab232496c3e619cc998913494f6d
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2021-11-07 22:41:17 -0300

    using try and if as expression

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,9 +1,32 @@
 package br.com.alura.bytebank
 
 import br.com.alura.bytebank.models.Address
+import java.lang.NumberFormatException
 
 fun main() {
     println("start main")
+    val stringNumber: String = "1.9"
+
+    val receivedValue: Double? = try {
+        stringNumber.toDouble()
+    } catch (e: NumberFormatException) {
+        null
+    }
+
+    val valueWithFee = if (receivedValue !== null) {
+        receivedValue + 0.1
+    } else {
+        null
+    }
+
+    if(receivedValue != null) {
+        println("Received value: $receivedValue")
+    }
+
+    if(valueWithFee != null) {
+        println("Value with fee: $valueWithFee")
+    }
+
     function1()
     println("end main")
 }
@@ -13,9 +36,6 @@ fun function1() {
     try {
         function2()
     } catch (e: ClassCastException) {
-        println(e.message)
-        println(e.stackTrace)
-        println(e.cause)
         e.printStackTrace()
         println("ClassCastException")
     }
