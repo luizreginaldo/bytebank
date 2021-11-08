9b9417716c5627a360a47291ca11a9bd772e3d8a
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2021-11-08 08:41:45 -0300

    working with elvis operator

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -3,10 +3,14 @@ package br.com.alura.bytebank
 import br.com.alura.bytebank.models.Address
 
 fun main() {
-    val address: Address? = null
+    val address: Address? = Address(address = "Rua A")
     println(address?.address?.length)
 
     address?.let { address: Address ->
         println(address.address.length)
+
+        val lengthComplementWithElvisOperator: Int = address.complement?.length ?: 0
+
+        println(lengthComplementWithElvisOperator)
     }
 }
